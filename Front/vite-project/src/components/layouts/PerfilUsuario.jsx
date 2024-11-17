e44954ed4f11843f225ce9f9b8117b73ea8b8a9a
matheusgoissouza <matheus8gois@gmail.com>
2024-11-17 00:27:05 -0300

    Mudança no TextoPostagem

--- Front/vite-project/src/components/layouts/PerfilUsuario.jsx
+++ Front/vite-project/src/components/layouts/PerfilUsuario.jsx
@@ -5,7 +5,7 @@ function PerfilUsuario() {
     <div className="perfil-usuario">
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVMfWvzBOAJx9M-P7VDSrT0PhGP3UqTEjDNQp-6twHXvACCwhouMWmVTFEOujDYt6pIz0&usqp=CAU" alt="Foto do usuário" className="foto-usuario" />
       <p>Matheus Gois</p>
-      <p>Matheus Gois</p>
+      <p>Olá, sou o Matheus e gosto muito de viajar</p>
       <button className="botao-adicionar">+</button>
     </div>
   );
--- Front/vite-project/src/components/layouts/TextoPostagem.jsx
+++ Front/vite-project/src/components/layouts/TextoPostagem.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function TextoPostagem() {
   return (
     <div className="texto-postagem">
-      <h2>Rio de Janeiro</h2>
+      <h2 className='Titloo'>Rio de Janeiro</h2>
       <p><strong>Por Matheus Gois</strong></p>
       <p>31/10/2024</p>
       <p>Uma viagem ao Rio de Janeiro é sempre uma aventura incrível...</p>
