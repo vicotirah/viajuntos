05003938bea295b087147e5a1ffbcb382d8799c1
guedespedro <pedroguazevedo123@gmail.com>
2024-11-29 17:40:10 -0300

    axios funcionando com as requisições em 'NovoRegistro.jsx'

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ from flask_restful import Api, Resource
 from flask_cors import CORS
 import os
 
-app = Flask(__name__,template_folder=os.path.join(os.path.dirname(__file__), 'Front\vite-project\src\views'))
+app = Flask(__name__,template_folder=os.path.join(os.path.dirname(__file__), 'Front\vite-project\src\views\pages'))
 CORS(app)
+CORS(app, origins=["http://localhost:5173"])
 
 app.config.from_pyfile('config.py')
 
@@ -15,4 +16,3 @@ from views import *
 
 if __name__ == '__main__':
     app.run(debug=True)
-
